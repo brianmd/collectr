142dd6cb043eaf2493faf9f5d87d8d1ffcfad9db
bmd <brian@murphydye.com>
2015-07-19 20:28:12 -0600

    Run redis specs on when local redis server exists.

--- spec/redis/redis_hash_spec.rb
+++ spec/redis/redis_hash_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require 'redis/redis_hash'
 require 'hash_spec'
 
-describe Collectr::RedisHash do
-  it_behaves_like 'a hash'
+if redis_exists?
+	describe Collectr::RedisHash do
+	  it_behaves_like 'a hash'
+	end
 end
